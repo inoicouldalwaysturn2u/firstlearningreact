bea07e0b3df8b1d7392336e8c706d72011f2c1de
inoicouldalwaysturn2u <iknowicouldalwaysturn@gmail.com>
2018-08-24 11:47:39 -0400

    Up to “Section 4, Lecture 33”

--- src/playground/counter-example.1.js
+++ src/playground/counter-example.1.js
@@ -8,35 +8,51 @@ class Counter extends React.Component {
     super(props);
 
     this.handleAddOne = this.handleAddOne.bind(this);
-    this.handleMinusOne = this.handleAddOne.bind(this);
-    this.handleReset = this.handleAddOne.bind(this);
+    this.handleMinusOne = this.handleMinusOne.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+
+    this.state = {
+      count: 0
+    };
   }
 
   handleAddOne() {
-    console.log('+1');
+    this.setState( ( prevState ) => {
+      return {
+        count: prevState.count + 1
+      };
+    } );
   }
 
   handleMinusOne() {
-    console.log('-1');
+    this.setState( ( prevState ) => {
+      return {
+        count: prevState.count - 1
+      };
+    });
   }
 
   handleReset() {
-    console.log('reset');
+    this.setState( () => {
+      return {
+        count: 0
+      };
+    });
   }
 
   render() {
     return (
       <div>
         <h1>
-          Counter:
+          Count: { this.state.count }
         </h1>
-        <button onClick={this.handleAddOne}>
+        <button onClick={ this.handleAddOne }>
           +1
         </button>
-        <button onClick={this.handleMinusOne}>
+        <button onClick={ this.handleMinusOne }>
           -1
         </button>
-        <button onClick={this.handleReset}>
+        <button onClick={ this.handleReset }>
           reset
         </button>
       </div>
