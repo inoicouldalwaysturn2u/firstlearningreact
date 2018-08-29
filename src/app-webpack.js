1027bb851c9e5d21182a825d9b873d4c070470e7
inoicouldalwaysturn2u <iknowicouldalwaysturn@gmail.com>
2018-08-29 11:50:14 -0400

    Up to “Section 6, Lecture 51: ES6 import/export”

--- src/app-webpack.js
+++ src/app-webpack.js
@@ -1,1 +1,12 @@
-console.log( 'lulz!' );
+// import './utils.js';
+
+import { square, add, subtract } from './utils.js';
+
+console.log( 'app-webpack lulz!' );
+
+console.log( square( 4 ) );
+
+console.log( add( 6, 9 ) );
+
+console.log( subtract( 6, 9 ) );
+
---
+++ src/utils.js
@@ -0,0 +1,14 @@
+console.log( 'util lulz!' );
+
+// second way
+export const subtract = ( a, b ) => a - b;
+
+// first way..down later
+const square = (x) => {
+  return x * x;
+}
+
+const add = (a, b) => a + b;
+
+// first way, here
+export { square, add };
