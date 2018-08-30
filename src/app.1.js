5f377236051b2972bc70992f126f9f879f059534
inoicouldalwaysturn2u <iknowicouldalwaysturn@gmail.com>
2018-08-30 07:16:42 -0400

    Part of “Section 6, Lecture 54: Setting up Babel with Webpack” #debugging. Can’t get it working. Maybe it is because of the mix of yarn and npm/package.json?

---
+++ src/app.1.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const template = (
+  <p>Showing the details</p>
+);
+
+ReactDOM.render(template, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require( 'path' );
 
 module.exports = {
-  entry: './src/app.js',
+  entry: path.join( __dirname, 'src/app.js' ),
   output: {
     path: path.join( __dirname, 'public/assets' ),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/
+    }]
   }
 };
