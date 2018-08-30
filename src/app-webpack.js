f8355a2fab165d243da372858addca24bc38b144
inoicouldalwaysturn2u <iknowicouldalwaysturn@gmail.com>
2018-08-29 20:28:54 -0400

    Up to “Section 6, Lecture 52” - Default Exports

--- src/app-webpack.js
+++ src/app-webpack.js
@@ -1,6 +1,7 @@
 // import './utils.js';
 
-import { square, add, subtract } from './utils.js';
+// import nameWhatYouWant, { square, add } from './utils.js';
+import subtract, { square, add } from './utils.js';
 
 console.log( 'app-webpack lulz!' );
 
@@ -9,4 +10,3 @@ console.log( square( 4 ) );
 console.log( add( 6, 9 ) );
 
 console.log( subtract( 6, 9 ) );
-
--- src/utils.js
+++ src/utils.js
@@ -6,9 +6,9 @@ export const subtract = ( a, b ) => a - b;
 // first way..down later
 const square = (x) => {
   return x * x;
-}
+};
 
 const add = (a, b) => a + b;
 
 // first way, here
-export { square, add };
+export { square, add, subtract as default };
