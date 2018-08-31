06cbb05dbf8a58cae6f57ef82263663d4d9382b6
Saadh <atextbooksituation@gmail.com>
2018-08-31 11:45:38 -0400

    Up to Section 8, Lecture 66: "Reset That S#!*

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import IndecisionApp from './components/IndecisionApp';
 
+import 'normalize.css/normalize.css';
+
 import './assets/style.scss';
 
 ReactDOM.render( <IndecisionApp />, document.getElementById( 'app' ) );
